39c2e64e5c632e7066b029cd73b5d034f3c3260c
fidadoma <filip.dechterenko@gmail.com>
2020-07-04 15:39:44 +0200

    descriptive statistics and contingency tables

--- utils.R
+++ utils.R
@@ -22,6 +22,14 @@ extract_values <- function(df_var) {
   matrix(r, ncol = nrow(df_var))
 }
 
+get_factor_levels <- function(v) {
+  str_split(v,"\\|", simplify = T) %>% c()
+}
+
+get_n_factor_levels <- function(v) {
+  str_split(v,"\\|", simplify = T) %>% length()
+}
+
 compute_mean <- function(v,pdist) {
   ranges <- extract_ranges_vec(v)
   if(pdist == "unif") {
@@ -56,6 +64,30 @@ generate_correlated_data <- function(n,r) {
   MASS::mvrnorm(n=n, mu=c(0, 0), Sigma=matrix(c(1, r, r, 1), nrow=2), empirical=TRUE)
 }
 
+generate_contingency_table <- function(n, r, x_lev, y_lev) {
+  
+  df <- generate_correlated_data(n,r)
+  colnames(df) <- c("x","y")
+  
+  x <- df[,1]
+  range_x <- range(x)
+  x_cutoffs <- runif(x_lev-1, min=range_x[1], max=range_x[2]) %>% sort()
+  x_categ <- rowSums(matrix(rep(x, x_lev-1), ncol = x_lev-1) >x_cutoffs)
+  
+  m1 <- table(x_categ)
+  m_out <- matrix(NA, ncol = x_lev, nrow = y_lev)
+  df2 <- df %>% as_tibble() %>% mutate(x_categ = x_categ)
+  for (i in 1:length(m1)) {
+    y <- df2 %>% filter(x_categ == (i-1)) %>% pull(y)
+    range_y <- range(y)
+    y_cutoffs <- runif(y_lev-1, min=range_y[1], max=range_y[2]) %>% sort()
+    m2 <- table(rowSums(matrix(rep(y, y_lev-1), ncol = y_lev-1) >y_cutoffs))
+    m_out[,i] <- m2                    
+  }
+  m_out
+  
+}
+
 compute_sd <- function(v,pdist) {
   ranges <- extract_ranges_vec(v)
   if(pdist == "unif") {
@@ -76,4 +108,12 @@ rescale_var <- function(x, m, sd) {
 check_var <- function(x,x_lower,x_upper) {
   x>=x_lower&x<=x_upper
   
+}
+
+lower_quartile <- function(x) {
+  quantile(x,probs = 0.25)
+}
+
+upper_quartile <- function(x) {
+  quantile(x,probs = 0.75)
 }
