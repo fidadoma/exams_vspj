b7862a353755503745e8baa3b877f242b253bd27
fidadoma <filip.dechterenko@gmail.com>
2020-08-06 21:02:59 +0200

    small bugfixes in probability distributions

--- generate_exam.R
+++ generate_exam.R
@@ -16,3 +16,29 @@ exams2html(list("contingency_tables/contingency_tables.Rmd",
            question = question_txt, 
            solution = solution_txt 
            )
+
+questions <- c("průměr", "median", "směrodatnou odchylku", "rozptyl", "dolní kvartil", "horní kvartil")
+ix <- sample(1:length(questions),3)
+
+exams2html(list(expar("desc_stat/desc_var.Rmd",ix = ix[1]),
+                expar("desc_stat/desc_var.Rmd",ix = ix[2]),
+                expar("desc_stat/desc_var.Rmd",ix = ix[3]),
+                "desc_stat/desc_plot.Rmd",
+                "desc_stat/density_plot.Rmd"
+                ),  
+           mathjax = T, 
+           question = question_txt, 
+           solution = solution_txt 
+)
+
+exams2html(list("probability/binomial_distribution.Rmd",
+                "probability/poisson_distribution.Rmd",
+                "probability/normal_distribution.Rmd",
+                "probability/normal_distribution_mean.Rmd",
+                "probability/normal_distribution_perc.Rmd",
+                "probability/normal_distribution_range.Rmd",
+                "probability/normal_distribution_sum.Rmd"),  
+mathjax = T, 
+question = question_txt, 
+solution = solution_txt 
+)
--- utils_probability.R
+++ utils_probability.R
@@ -2,19 +2,19 @@ probability_binomial_setting <- function(n, k, pi_param, quantifier) {
   
     if (quantifier == "právě") {
       solution <- dbinom(k, n, pi_param)
-      explanation <- sprintf("V tomto případě nás zajímá P(X=%d). Dosadíme do vzorce pro binomické rozdělení s parametry \\pi=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, pi_param, k, n, solution)
+      explanation <- sprintf("V tomto případě nás zajímá P(X=%d). Dosadíme do vzorce pro binomické rozdělení s parametry $\\pi$=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, pi_param, k, n, solution)
     } else if (quantifier == "alespoň") {
       solution <- 1 - (pbinom(k, n, pi_param) - dbinom(k, n, pi_param))
-      explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - P(X < %d) = 1 - P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry \\pi=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k, k - 1, pi_param, k, n, solution)
+      explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - P(X < %d) = 1 - P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry $\\pi$=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k, k - 1, pi_param, k, n, solution)
     } else if (quantifier == "nejvýše") {
       solution <- pbinom(k, n, pi_param)
-      explanation <- sprintf("V tomto případě nás zajímá P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry \\pi=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, pi_param, k, n, solution)
+      explanation <- sprintf("V tomto případě nás zajímá P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry $\\pi$=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, pi_param, k, n, solution)
     } else if (quantifier == "méně než") {
       solution <- pbinom(k - 1, n, pi_param)
-      explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry \\pi=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k - 1, pi_param, k, n, solution)
+      explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry $\\pi$=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k - 1, pi_param, k, n, solution)
     } else if (quantifier == "více než") {
       solution <- 1 - pbinom(k, n, pi_param)
-      explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry \\pi=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k, pi_param, k, n, solution)
+      explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - P(X <= %d). Dosadíme do vzorce pro binomické rozdělení s parametry $\\pi$=%s, k=%d, n=%d. Pravděpodobnost je %.3f", k, k, pi_param, k, n, solution)
     } else {
       
       stop(sprintf("incorrect settings: k=%d, quantifier = '%s'",k, quantifier))
@@ -28,19 +28,19 @@ probability_poisson_setting <- function(lambda, k, quantifier) {
   
   if (quantifier == "právě") {
     solution <- dpois(k, n)
-    explanation <- sprintf("V tomto případě nás zajímá P(X=%d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry \\lambda=%d, k=%d. Pravděpodobnost je %.3f", k, lambda, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X=%d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry $\\lambda$=%d, k=%d. Pravděpodobnost je %.3f", k, lambda, k, solution)
   } else if (quantifier == "alespoň") {
     solution <- 1 - (ppois(k, lambda) - dpois(k, lambda))
-    explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - P(X < %d) = 1 - P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry \\lambda=%d, k=%d. Pravděpodobnost je %.3f", k, k, k - 1, lambda, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - P(X < %d) = 1 - P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry $\\lambda$=%d, k=%d. Pravděpodobnost je %.3f", k, k, k - 1, lambda, k, solution)
   } else if (quantifier == "nejvýše") {
     solution <- ppois(k, lambda)
-    explanation <- sprintf("V tomto případě nás zajímá P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry \\lambda=%d, k=%d. Pravděpodobnost je %.3f", k, lambda, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry $\\lambda$=%d, k=%d. Pravděpodobnost je %.3f", k, lambda, k, solution)
   } else if (quantifier == "méně než") {
     solution <- ppois(k - 1, lambda)
-    explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry \\lambda=%d, k=%d. Pravděpodobnost je %.3f", k, k - 1, lambda, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry $\\lambda$=%d, k=%d. Pravděpodobnost je %.3f", k, k - 1, lambda, k, solution)
   } else if (quantifier == "více než") {
     solution <- 1 - ppois(k, lambda)
-    explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry \\lambda=%d, k=%d. Pravděpodobnost je %.3f", k, k, lambda, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - P(X <= %d). Dosadíme do vzorce pro Poissonovo rozdělení s parametry $\\lambda$=%d, k=%d. Pravděpodobnost je %.3f", k, k, lambda, k, solution)
   } else {
     
     stop(sprintf("incorrect settings: k=%d, quantifier = '%s'",lambda, quantifier))
@@ -55,16 +55,16 @@ probability_normal_setting <- function(k, mu_param, sigma_param, quantifier) {
   
   if (quantifier == "alespoň") {
     solution <- 1 - (pnorm(k, mu_param, sigma_param))
-    explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - F(%d). Dosadíme do vzorce pro normální rozdělení s parametry \\mu=%d, \\sigma=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X >= %d) = 1 - F(%d). Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
   } else if (quantifier == "nejvýše") {
     solution <- pnorm(k, mu_param, sigma_param)
-    explanation <- sprintf("V tomto případě nás zajímá P(X <= %d) = F(%d). Dosadíme do vzorce pro normální rozdělení s parametry \\mu=%d, \\sigma=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X <= %d) = F(%d). Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
   } else if (quantifier == "méně než") {
     solution <- pnorm(k, mu_param, sigma_param)
-    explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = F(%d). Dosadíme do vzorce pro normální rozdělení s parametry \\mu=%d, \\sigma=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X < %d) = F(%d). Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
   } else if (quantifier == "více než") {
     solution <- 1 - pnorm(k, mu_param, sigma_param)
-    explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - F(X%d). Dosadíme do vzorce pro normální rozdělení s parametry \\mu=%d, \\sigma=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
+    explanation <- sprintf("V tomto případě nás zajímá P(X > %d) = 1 - F(X%d). Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. Pravděpodobnost je %.3f", k, k, mu_param, sigma_param, k, solution)
   } else {
     
     stop(sprintf("incorrect settings: k=%d, quantifier = '%s'",k, quantifier))
@@ -78,7 +78,7 @@ probability_normal_setting_range <- function(k_lower, k_upper, mu_param, sigma_p
   
   
   solution <- pnorm(k_upper, mu_param, sigma_param) - pnorm(k_lower, mu_param, sigma_param)
-  explanation <- sprintf("Máme-li v zadání rozsah hodnot, použijeme vzorec P(X > větší hodnota) - P(X < menší hodnota). V tomto případě tedy použijeme P(X > %d) - P(X < %d) = F(%d) - F(%d). Dosadíme do vzorce pro normální rozdělení s parametry \\mu=%d, \\sigma=%d,  k=%d a k=%d. Pravděpodobnost je %.3f", k_upper, k_lower, k_upper, k_lower, mu_param, sigma_param, k_lower, k_upper, solution)
+  explanation <- sprintf("Máme-li v zadání rozsah hodnot, použijeme vzorec P(X > větší hodnota) - P(X < menší hodnota). V tomto případě tedy použijeme P(X > %d) - P(X < %d) = F(%d) - F(%d). Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d a k=%d. Pravděpodobnost je %.3f", k_upper, k_lower, k_upper, k_lower, mu_param, sigma_param, k_lower, k_upper, solution)
   
   l <- list(solution = solution, explanation=explanation)
   l
@@ -98,25 +98,25 @@ probability_normal_setting_sum <- function(k, n, mu_param, sigma_param, quantifi
   
   if (quantifier == "alespoň") {
   solution <- 1 - (pnorm(k, mu_param*n, sigma_param*n))
-  explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_sum = n*\\mu$ a $\\sigma_sum = n*\\sigma$. 
+  explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_{sum} = n*\\mu$ a $\\sigma_{sum} = n*\\sigma$. 
                          V tomto případě nás zajímá P(X >= %d) = 1 - F(%d). 
                          Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. 
                          Pravděpodobnost je %.3f", k, k, n*mu_param, n*sigma_param, k, solution)
   } else if (quantifier == "nejvýše") {
     solution <- pnorm(k, mu_param*n, sigma_param*n)
-    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_sum = n*\\mu$ a $\\sigma_sum = n*\\sigma$. 
+    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_{sum} = n*\\mu$ a $\\sigma_{sum} = n*\\sigma$. 
                          V tomto případě nás zajímá P(X <= %d) = F(%d). 
                          Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. 
                          Pravděpodobnost je %.3f", k, k, n*mu_param, n*sigma_param, k, solution)
   } else if (quantifier == "méně než") {
     solution <- pnorm(k, mu_param*n, sigma_param*n)
-    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_sum = n*\\mu$ a $\\sigma_sum = n*\\sigma$. 
+    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_{sum} = n*\\mu$ a $\\sigma_{sum} = n*\\sigma$. 
                          V tomto případě nás zajímá P(X < %d) = F(%d). 
                          Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. 
                          Pravděpodobnost je %.3f", k, k, n*mu_param, n*sigma_param, k, solution)
   } else if (quantifier == "více než") {
     solution <- 1 - pnorm(k, mu_param*n, sigma_param*n)
-    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_sum = n*\\mu$ a $\\sigma_sum = n*\\sigma$. 
+    explanation <- sprintf("Pro sumu normálních rozdělení platí, že $\\mu_{sum} = n*\\mu$ a $\\sigma_{sum} = n*\\sigma$. 
                          V tomto případě nás zajímá P(X < %d) = F(%d). 
                          Dosadíme do vzorce pro normální rozdělení s parametry $\\mu$=%d, $\\sigma$=%d,  k=%d. 
                          Pravděpodobnost je %.3f", k, k, n*mu_param, n*sigma_param, k, solution)
